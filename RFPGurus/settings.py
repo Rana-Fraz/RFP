c51eefff15bd90d9ef89e2b2ca016fd6bf68c627
rana fraz <rana.fraz.khan@gmail.com>
2019-06-15 11:43:29 +0500

    second

--- RFPGurus/settings.py
+++ RFPGurus/settings.py
@@ -231,3 +231,6 @@ PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
