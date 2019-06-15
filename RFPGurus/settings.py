fbb4bf896cfa1ace246ad2d641aab53d9517cc03
rana fraz <rana.fraz.khan@gmail.com>
2019-06-15 14:59:24 +0500

    setting

--- RFPGurus/settings.py
+++ RFPGurus/settings.py
@@ -92,6 +92,7 @@ JWT_AUTH = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
